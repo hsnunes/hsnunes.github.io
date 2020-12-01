e9ead7b1a3e406668e8e72af6ad8eae778fb2cf7
Hilder Nunes <hildersn@gmail.com>
2020-12-01 17:00:40 -0300

    criando um painel de aplicaçoes

--- templates/admin/pages.tpl.php
+++
@@ -1,3 +0,0 @@
-<h3>Criacao de Paginas</h3>
-
-<a href="/admin">início</a>
--- templates/admin/painel.tpl.php
+++ templates/admin/painel.tpl.php
@@ -1,40 +1,24 @@
-<main role="main">
+<main role="main" class="mt-3">
 
-  <!-- Main jumbotron for a primary marketing message or call to action -->
-  <div class="jumbotron">
-    <div class="container">
-      <h1 class="display-3">Buenos!</h1>
-      <p>Aqui ficarão avisos importantes ou tarefas de total prioridade.</p>
-      <p><a class="btn btn-primary btn-lg" href="#" role="button">verificar &raquo;</a></p>
-    </div>
-  </div>
-
+  <section>
   <div class="container">
-    <!-- Example row of columns -->
-    <div class="row">
-      <div class="col-md-4">
-        <h2>Acesso</h2>
-        <p>Impementar o acesso de novos ao sistema. Verificando sempre o registro ZERO (ninguem cadastrado) </p>
-        <p><a class="btn btn-secondary" href="#" role="button">Detalhes &raquo;</a></p>
-      </div>
-      <div class="col-md-4">
-        <h2>Cliente</h2>
-        <p>Cadastro de clientes, como controle de serviços e equipamentos</p>
-        <p><a class="btn btn-secondary" href="#" role="button">Detalhes &raquo;</a></p>
-      </div>
-      <div class="col-md-4">
-        <h2>Orçamento</h2>
-        <p>Começar a elaborar um orçamento online para clientes, pensando sempre no QR code</p>
-        <p><a class="btn btn-secondary" href="#" role="button">Detalhes &raquo;</a></p>
+    <div class="titleSection">
+      <h2>Aplicações:</h2>
+    </div>
+
+    <div class="card text-white bg-secondary mb-3" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Cadastro de Clientes</h5>
+        <p class="card-text">Uma lista de clientes e seus contatos. Agregado a equipamentos e serviços.</p>
+        <a href="#" class="btn btn-primary">Iniciar</a>
       </div>
     </div>
 
-    <hr>
-
-  </div> <!-- /container -->
+  </div>
+  </section>
 
 </main>
 
-<footer class="container">
+<footer class="container mt-3">
   <p>&copy; Hsnunes <i><b>Tec&Dev</b></i> 2020</p>
 </footer>
